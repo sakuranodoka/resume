3a9d057af1afd5bdb11c35e6f480bfec928a4290
sakuranodoka <atikom.sn@gmail.com>
2020-09-02 02:22:04 +0700

    Remove top router and edit some word : Athikom

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="@/assets/logo.png">
+    <br />The world is mine. <br />
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
